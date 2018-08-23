47fee6cc0afb7c38616e5e224cc94f8633a8aae6
I'MNOTURFUCKINGBITCHㅗㅗ <hys2rang@gmail.com>
2018-08-23 23:25:46 +0900

    printing triangle1

---
+++ multiple loops/Printing_Triangle_1.c
@@ -0,0 +1,24 @@
+﻿/*
+예제 입력
+3
+예제 출력
+*
+**
+***
+*/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<i+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
